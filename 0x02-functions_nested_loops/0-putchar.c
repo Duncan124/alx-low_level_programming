910db4a79bdb98e623a4738a01921bbf45a58405
Duncan124 <esigloria32@gmail.com>
2022-12-13 18:32:16 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (Success)
+ */
+ int main (void)
+ {
+	 char ch ='1';
+
+	 _putchar (ch, '\n');
+
+         return (0);
+ }	 
