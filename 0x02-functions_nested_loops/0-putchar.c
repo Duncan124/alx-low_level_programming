262e0f47e339117e3d1ab6af930b6275dc12d8a6
Duncan124 <esigloria32@gmail.com>
2022-12-14 07:40:26 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Prints Holberton as a message.
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
- int main (void)
- {
-	 char ch=(main, '1');
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-	 _putchar (ch, '\n');
-
-         return (0);
- }	 
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
