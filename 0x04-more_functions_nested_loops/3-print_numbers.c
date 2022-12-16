c591809ac6540ea4144c845579ab39acb0dfd071
Duncan124 <esigloria32@gmail.com>
2022-12-16 14:04:58 -0800

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
-#include "main.h"
+include "main.h"
 
 /**
- * print_numbers - prints number
+ * print_numbers - prints numbers from 0 to 9
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
-	{
+	for (i = 48; i <= 57; i++)
 		_putchar(i);
-	}
+
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,8 +4,8 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-extern void print_numbers(void);
-extern void print_most_numbers(void);
+void print_numbers(void);
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
