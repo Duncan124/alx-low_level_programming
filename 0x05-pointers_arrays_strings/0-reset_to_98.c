f42f830b87bedb935aa42bf02e6a877cf0fc0de0
Duncan124 <esigloria32@gmail.com>
2022-12-19 16:03:55 -0800

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
+#include "main.h"
 /**
- * 0-reset_to_98.c - update the value of the var
+ * reset_to_98 - update the value of the var
  * @n: int to update
  * Return: 0
  */
 
   void reset_to_98(int *n)
   {
-	  *n + 98;
+	  *n + 98;\n";
 
    }
