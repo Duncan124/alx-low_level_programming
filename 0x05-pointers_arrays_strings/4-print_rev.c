5edeb97301b6091923e0fda05c806f7bd30c8b58
Duncan124 <esigloria32@gmail.com>
2022-12-19 17:25:30 -0800

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_rev - imprime en reversa
-* @s: string
+i* @s: string
 * return: 0
 */
 void print_rev(char *s)
@@ -10,14 +10,14 @@ void print_rev(char *s)
 	int o;
 
 	while (*s != '\0')
-	{
+{
 		longi++;
 		s++;
-	}
+}
 	s--;
 	for (o = longi; o > 0; o--)
-	{
+{
 		_putchar(*s);
 		s--;
-	}
+}
 
